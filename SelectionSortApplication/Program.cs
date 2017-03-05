8fb4933d1a313d62b42716c3ca1a4700fa5c6484
cshah <cshah@tavisca.com>
2017-03-05 23:02:13 +0530

    Inital Commit

---
+++ SelectionSortApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSortApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noOfElementInArray;
+            
+            Console.WriteLine("Enter the No. of Elements you want for selection Sort");
+            noOfElementInArray = Convert.ToInt32(Console.ReadLine());
+            int[] selectionSortArray = new int[noOfElementInArray];
+            Console.WriteLine("Enter the Elements in array");
+            for (int iterator = 0; iterator < noOfElementInArray; iterator++)
+            {
+                selectionSortArray[iterator] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            SortingController selectionObject = new SortingController();
+            int[] sortedArray = selectionObject.SelectionSortLogic(selectionSortArray, noOfElementInArray);
+            selectionObject.DisplayArray(sortedArray, noOfElementInArray);
+            Console.WriteLine("\n\nThankyou\n\n Press any key to Exit the Apllication...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SelectionSortApplication/SelectionSortAlgorithm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSortApplication
+{
+   public class SortingController
+    {
+       public int[] SelectionSortLogic(int[] selectionSortArray, int noOfElementInArray)
+        {
+            int minimunValue;
+
+            for(int index = 0; index < noOfElementInArray; index++)
+            {
+                minimunValue = index;
+                for(int nextIndex = index + 1; nextIndex < noOfElementInArray; nextIndex++)
+                {
+                    if(selectionSortArray[nextIndex] < selectionSortArray[minimunValue])
+                    {
+                        minimunValue = nextIndex;
+                    }
+                }
+                if(minimunValue != index)
+                {
+                   
+                    SwapNum(ref selectionSortArray[index],ref selectionSortArray[minimunValue]);
+                }
+            }
+
+
+            return selectionSortArray;
+
+          
+        }
+        void SwapNum(ref int fristNumber, ref int secondNumber)
+        {
+
+            int tempswap = fristNumber;
+            fristNumber = secondNumber;
+            secondNumber = tempswap;
+        }
+        public void DisplayArray(int[] selectionSortArray,int noOfElementInArray)
+        {
+            Console.WriteLine("Sorted Array\n");
+            for (int iterator = 0; iterator < noOfElementInArray; iterator++)
+            {
+                Console.Write(" " + selectionSortArray[iterator]);
+            }
+        }
+    }
+}
